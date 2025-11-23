f3bc559123b791f874e5de2e634155a705418b8a
marta997 <sunako.aome@gmail.com>
2025-11-24 00:52:38 +0100

    ✨ added a table to list if servers are working

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Header from "./components/Header"
 import Results from "./components/Results"
 import Error from "./components/Error"
 import Searcher from "./components/Searcher"
+import DenseTable from "./components/DenseTable"
 
 const darkTheme = createTheme({
   palette: {
@@ -19,8 +20,17 @@ const darkTheme = createTheme({
   },
 })
 
+const WORKING_ICONS = {
+  'yes': '✅',
+  'no': '❌',
+  'idk': '❔',
+}
+const WORKING_SITES = ['nyaasi', 'ybt']
+const rowNames = ['Server', 'Working']
+
 function App() {
   const [servers, setServers] = useState([])
+  const [rows, setRows] = useState([])
   // TODO: use a context or something?
   const [results, setResults] = useState([])
   const [loading, setLoading] = useState(false)
@@ -33,6 +43,13 @@ function App() {
   useEffect(() => {
     getServersData()
   }, [])
+
+  useEffect(() => {
+    const serverRows = servers.map((name) => (
+      [name, WORKING_SITES.includes(name) ? WORKING_ICONS['yes'] : WORKING_ICONS['idk']]
+    ))
+    setRows(serverRows)
+  }, [servers])
 
   return (
     <ThemeProvider theme={darkTheme}>
@@ -47,7 +64,7 @@ function App() {
           <Box sx={{ marginBottom: "1rem" }}>
             <Header />
           </Box>
-          <Box sx={{ marginBottom: "1rem" }}>
+          <Box sx={{ marginBottom: "2rem" }}>
             <Searcher
               servers={servers}
               setResults={setResults}
@@ -60,6 +77,9 @@ function App() {
           </Box>
           <Box>
             <Error />
+          </Box>
+          <Box sx={{ marginTop: '2rem' }}>
+            <DenseTable rowNames={rowNames} rows={rows} />
           </Box>
         </Box>
       </ErrorContextProvider>
---
+++ src/components/DenseTable.jsx
@@ -0,0 +1,42 @@
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import Paper from '@mui/material/Paper'
+
+export default function DenseTable(props) {
+  const {rowNames, rows} = props
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 450 }} size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+
+            {rowNames.map((name) => (
+              <TableCell key={name}>
+                {name}
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row, index) => (
+            <TableRow
+              key={index}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              {row.map((field, index) => (
+                <TableCell key={index}>
+                  {field}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
